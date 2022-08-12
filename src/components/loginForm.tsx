f837188377afb86141f9217e5a9b01e342642e83
Gabriel Salinas <g.szada@gmail.com>
2022-08-12 20:10:53 +0200

    css files are now modular

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,36 +1,36 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { handleChange, handleLogin } from '../handlers'
-import {ctx} from '../App'
-import '../index.css'
+import { ctx } from '../App'
+import s from './loginForm.module.css'
 
 export default function LoginForm() {
-  const {state, dispatch} = useContext(ctx)
+  const { state, dispatch } = useContext(ctx)
 
   return (
-    <form className='form-box'>
-        <label htmlFor='username'>
-            Username
-            <input 
-            type='text' 
-            name='username' 
-            onChange={(e) => handleChange(e, dispatch)} 
-            value={state.username} 
-            />
-        </label>
+    <form className={s.form_box}>
+      <label htmlFor='username'>
+        Username
+        <input
+          type='text'
+          name='username'
+          onChange={(e) => handleChange(e, dispatch)}
+          value={state.username}
+        />
+      </label>
 
-        <label htmlFor='password'>
-            Password
-            <input 
-            type='password' 
-            name='password' 
-            onChange={(e) => handleChange(e, dispatch)} 
-            value={state.password} 
-            />
-        </label>
+      <label htmlFor='password'>
+        Password
+        <input
+          type='password'
+          name='password'
+          onChange={(e) => handleChange(e, dispatch)}
+          value={state.password}
+        />
+      </label>
 
-        <button onClick={(e) => handleLogin(e, dispatch)}>
-            Login
-        </button>
+      <button onClick={(e) => handleLogin(e, dispatch)}>
+        Login
+      </button>
     </form>
   )
 }
